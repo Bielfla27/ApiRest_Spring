be8058ebcfe43189ef0ec529e4d07fc054ace6ca
Bielfla27 <gabibielmft@outlook.com>
2022-12-07 20:49:47 -0300

    Criando método get para retornar todos os médicos

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,14 +1,19 @@
 package med.voll.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.persistence.GeneratedValue;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 
@@ -23,5 +28,10 @@ public class MedicoController {
 	@Transactional // para ter uma transação ativa com o banco
 	public void  cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
 		medicoRepository.save(new Medico(dados));
+	}
+	
+	@GetMapping
+	public List<DadosListagemMedico> listar(){
+		return medicoRepository.findAll().stream().map(DadosListagemMedico::new).toList(); //chamando método listar todos jpa e convertendo o dadosListagemMedico para um tipo médico
 	}
 }
